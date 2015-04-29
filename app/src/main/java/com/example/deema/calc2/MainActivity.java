5e81e3d0819eb6841ca67d856b21bcb613e3fdd8
didris <dimaid@hotmail.com>
2015-04-29 11:46:44 +0300

    some changes

--- app/src/main/java/com/example/deema/calc2/MainActivity.java
+++ app/src/main/java/com/example/deema/calc2/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.deema.calc2;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.InputFilter;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,30 +11,12 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
     Double n1=0.0,n2=0.0,m;
-    int x;
     String operation;
-    TextView display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button backspace=(Button)findViewById(R.id.back);
-        backspace.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String str= display.getText().toString();
-                if (str.length() >1 ) {
-                    str = str.substring(0, str.length() - 1);
-                    display.setText(str);
-                }
-                else if (str.length() <=1 ) {
-                    display.setText("0");
-                }
-            }
-
-        });
     }
-
-
 
 
     @Override
@@ -43,15 +24,10 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -65,54 +41,43 @@ public class MainActivity extends ActionBarActivity {
 
     public void onClick_zero(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
-
+        display.setText(display.getText().toString()+0);
     }
     public void onClick_one(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-       display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+1);
     }
     public void onClick_two(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+2);
     }
     public void onClick_three(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+3);
     }
     public void onClick_four(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+4);
     }
     public void onClick_five(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+5);
     }
     public void onClick_six(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+6);
     }
     public void onClick_seven(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+7);
     }
     public void onClick_eight(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+8);
     }
     public void onClick_nine(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        String buttonPressed = ((Button) v).getText().toString();
-        display.setText(display.getText().toString() +buttonPressed);
+        display.setText(display.getText().toString()+9);
     }
 
     public void on1(View v ){
@@ -128,7 +93,6 @@ public class MainActivity extends ActionBarActivity {
         TextView display= (TextView) findViewById(R.id.textView1);
         if(o.equals("+")){
             display.setText(n1+n2+"");
-
         }
         else if(o.equals("-")){
             display.setText(n1-n2+"");
@@ -167,8 +131,6 @@ public class MainActivity extends ActionBarActivity {
 
     public void onClick_subtract(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        int maxLength = 8;
-        display.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
         Button b=(Button)findViewById(R.id.buttonSubtract);
         operation=b.getText().toString();
         n1=Double.parseDouble(display.getText().toString());
@@ -176,17 +138,16 @@ public class MainActivity extends ActionBarActivity {
     }
     public void onClick_clear(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
-        display.setText("0");
+        display.setText("");
 
     }
 
-    /*public void onClick_back(View v){
+    public void onClick_back(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
         String str=display.getText().toString().substring(0,display.getText().toString().length()-1);
-        display.setText(str);}*/
+        display.setText(str);
 
-
-
+    }
     public void onClick_n(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
         Double num=Double.parseDouble(display.getText().toString());
@@ -196,7 +157,6 @@ public class MainActivity extends ActionBarActivity {
     public void onClick_m(View v){
         TextView display= (TextView) findViewById(R.id.textView1);
         m=Double.parseDouble(display.getText().toString());
-        display.setText("0");
 
 
     }
